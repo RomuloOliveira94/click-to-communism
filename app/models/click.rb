482849c355e3e64488e74efd840caa50c1a28e69
Rômulo Olveira <romuloffall@gmail.com>
2025-09-13 14:14:05 -0300

    #add realtime update on total clicks

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,4 +1,6 @@
 class Click < ApplicationRecord
+  after_create_commit { broadcast_replace_to :clicks, target: "clicks_counter", partial: "home/clicks_counter", locals: { clicks_count: Click.count } }
+
   validates :ip_address, presence: true
   validates :user_agent, presence: true
 end
